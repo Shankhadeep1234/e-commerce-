3daf13565716702cd911dc8a06338e7e1e68c724
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-07-23 10:30:22 +0530

    added cartIcon and implemented redux for it

--- src/components/redux/user/user.actions.js
+++ src/components/redux/user/user.actions.js
@@ -1,6 +1,6 @@
-export const setCurrentUser = (user) => {
-  return {
-    type: "SET_CURRENT_USER",
-    payload: user,
-  };
-};
+import { UserActionTypes } from "./user.types";
+
+export const setCurrentUser = (user) => ({
+  type: UserActionTypes.SET_CURRENT_USER,
+  payload: user,
+});
