b252d2f7097ce67e517aa04a36dbb2ae7ce4eb40
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-07-23 13:37:46 +0530

    update cart-utils and shop data

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-import { setCurrentUser } from "./components/redux/user/user.actions";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
+import store from "./redux/store";
+
 import "./index.css";
 import App from "./App";
-import store from "./components/redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
